cf27e9f502be9a2d97453b538838b75ebdb8701f
Robert Garcia <robertserranogar@gmail.com>
2023-09-18 14:01:45 -0600

    changed and debugged the accordion app

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -7,60 +7,55 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const Counter = (props) => {
-    return (
-        <div>
-            <button onClick={props.minus}>Minus</button>
-            <div>{props.value}</div>
-            <button onClick={props.plus}>Plus</button>
-        </div>
-    );
+  return (
+    <div>
+      <button onClick={props.minus}>Minus</button>
+      <div>{props.value}</div>
+      <button onClick={props.plus}>Plus</button>
+    </div>
+  );
 };
 
 export default Counter;
 
 
+
 // App.js
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 import Counter from "./Counter";
 
 const App = () => {
-    const [number, setNumber] = useState(0);
-    let result;
+  const [number, setNumber] = useState(0);
+  let result;
 
-    // create a main function that holds both functions with conditions 
-    const add = () => {
-        if (number > 0) {
-            result = number + 1;
-            setNumber(result)
-        } else {
-
-        } 
+  // create a main function that holds both functions with conditions
+  const add = () => {
+    if (number >= 0) {
+      result = number + 1;
+      setNumber(result);
+    } else {
     }
+  };
 
-    const substract = () => {
-        if (number > 0) {
-            result = number - 1;
-            setNumber(result);
-        } else {
-            
-        }
+  const substract = () => {
+    if (number > 0) {
+      result = number - 1;
+      setNumber(result);
+    } else {
     }
+  };
 
-    return (
-        <Counter 
-        minus={substract}
-        value={number}
-        plus={add}
-        />
+  return (
+    <Counter minus={substract} value={number} plus={add} />
+  )
+};
 
-    );
-}
+export default App;
 
 
 // index.js
-
 import React from "react";
 import ReactDOM from "react-dom";
 
